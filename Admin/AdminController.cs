504541d99e1f8801746ca79f8f6ffab2359e6719
huysocho <161503841+huysocho@users.noreply.github.com>
2024-12-10 15:08:50 +0700

    Add files via upload

---
+++ Admin/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCamping.Controllers.Admin
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
