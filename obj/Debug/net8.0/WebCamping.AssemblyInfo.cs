dc3f244ecec1878f921d3a349c51fa204fae7520
PhanQuocThuan <phanquocthuan1812@gmail.com>
2024-12-17 13:08:56 +0700

    up index+1

--- obj/Debug/net8.0/WebCamping.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebCamping.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCamping")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6a46ce5c0e8a4f712c8d0d8abba7186cab9d250")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b7b36e4754197225aefaa813b08521124d6b1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCamping")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCamping")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
