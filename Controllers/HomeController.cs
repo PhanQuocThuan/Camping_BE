96895af7e5271199c1413ad2b791dda7e8aa8c43
PhanQuocThuan <phanquocthuan1812@gmail.com>
2024-12-17 16:17:05 +0700

    update k1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,19 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using WebCamping.Models;
 
 namespace CampingEquipment.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly CampingContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(CampingContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            ViewData["Brands"] = _context.Brands.AsNoTracking()
+                 .Include(x => x.Products.OrderBy(y => y.Name))
+                 .OrderBy(x => x.BRA_ID).ToList();
+
+            ViewData["HostProducts"] = _context.Products.AsNoTracking()
+                .Include(x => x.Brand)
+                .OrderBy(x => x.Price).ToList();
             return View();
         }
 
--- obj/Debug/net8.0/WebCamping.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebCamping.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCamping")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b7b36e4754197225aefaa813b08521124d6b1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc3f244ecec1878f921d3a349c51fa204fae7520")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCamping")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCamping")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
